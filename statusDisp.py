0e57fd30bd97b99d511cd1cb11ca76aaaab458a5
Mark Rodighiero <mark.rodighiero@amadamiyachi.com>
2019-12-04 13:05:24 -0800

    Ugly hack attempt to eliminate screen blanking

--- statusDisp.py
+++ statusDisp.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 from showip.showIP import ShowIP
 import subprocess
+from subprocess import Popen, PIPE
 import shlex
 import sys, os
 from time import sleep
@@ -25,19 +26,39 @@ def makeImage():
     d.text((8,180), "Hostname: {}".format(hname), fill=(255,255,255), font=font)
     img.save('pil_text.png')
 
-def showImage():
-    cmd = 'fbi -d /dev/fb0 -noverbose -nocomments -T 1 -t 5 -cachemem 0  pil_text.png image1.png image2.png'
-    args = shlex.split(cmd)
-    sppid = subprocess.run(args)
-#    print("subprocess pid = {}".format(sppid.pid))
+def testActive():
+    cmd1 = 'ps aux'
+    cmd2 = 'grep fbi' 
+    cmd3 = 'grep -o noverbose'
+    cmds = [cmd1, cmd2, cmd3]
+    p = [0 for c in cmds]
+    for i,c in enumerate(cmds):
+       args = shlex.split(c)
+       if i == 0: # first cmd
+           p[i] = Popen(args, stdout=PIPE)
+       else:
+           p[i] = Popen(args, stdin=p[i-1].stdout, stdout=PIPE)
+    p[0].stdout.close()
+    resp = str(p[len(p)-1].communicate()[0],'utf-8')
+ #   print("test output: {}, bool:{}".format(resp, 'noverbose' in resp))
+    return 'noverbose' in resp
+
 def main():
-    cmd = 'fbi -d /dev/fb0 -noverbose -nocomments -T 1 -t 2 -cachemem 0  pil_text.png image1.png image2.png'
+    cmd = 'fbi -d /dev/fb0 -noverbose -nocomments -T 1 -t 2 -cachemem 0 pil_text.png pil_text.png pil_text.png'
     args = shlex.split(cmd)
-    sppid = subprocess.run(args)
-#    print("subprocess pid = {}".format(sppid.pid))
+    sppid = subprocess.run(args, timeout=None)
+
     while True:
        makeImage()
-       sleep(2)
+       sleep(1)
+       testRes = testActive()
+
+       if not testRes:
+          print("test output Failed: {}".format(testRes))
+          cmd = 'fbi -d /dev/fb0 -noverbose -nocomments -T 1 -t 2 -cachemem 0 pil_text.png pil_text.png pil_text.png'
+          args = shlex.split(cmd)
+          sppid = subprocess.run(args, timeout=None)
+
 
 if __name__ == "__main__":
     main()
