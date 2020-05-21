82430321596c55abb5764338efdec255c6beb614
tmcolby <tyson.colby@innotech.engineering>
2020-05-21 08:30:48 -0700

    wip

--- make.py
+++ make.py
@@ -3,8 +3,10 @@ canvasW, canvasH =(320, 240)
 green = (34, 177, 76)
 red = (222, 60, 60)
 white = (255, 255, 255)
+grey = (181, 181, 181)
+black = (0,0,0)
 
-bg = Image.new('RGB', (canvasW, canvasH), green)
+bg = Image.new('RGB', (canvasW, canvasH), grey)
 
 logo = Image.open("awt_logo.png")
 logoW, logoH = logo.size
@@ -22,7 +24,7 @@ verticalLineSpace = 0.3 # multiplier ie: .3 means 30%
 
 lineSpaceV = int((canvasH - (logoPastePos[1] + logoH)) / (fontSize+(fontSize*verticalLineSpace)))
 for i in range(lineSpaceV):
-    textOverlay.text((5, logoH+5+i*(fontSize+(fontSize*verticalLineSpace))), line, fill=white, font=font)
+    textOverlay.text((5, logoH+5+i*(fontSize+(fontSize*verticalLineSpace))), line, fill=black, font=font)
 
 
 bg.show()
