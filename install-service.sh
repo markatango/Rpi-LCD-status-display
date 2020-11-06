aaf1c132924ae180399098a3966b4937c2c0f8c6
Mark Rodighiero <markatango@gmail.com>
2020-11-06 11:37:15 -0800

    displayed different screens at reboot and shutdown

--- install-service.sh
+++ install-service.sh
@@ -4,12 +4,15 @@ STATUS_SERVICE=amya-logo-2.service
 SPLASH_SERVICE=splashscreen.service
 SPLASH_SHUTTINGDOWN_SERVICE=shuttingdown_lcd.service
 SPLASH_HALTED_SERVICE=halted_lcd.service
+SPLASH_FINAL_SERVICE=final_lcd.service
+
 DEST=/lib/systemd/system
 
 sudo cp $STATUS_SERVICE $DEST
 sudo cp $SPLASH_SERVICE $DEST
 sudo cp $SPLASH_SHUTTINGDOWN_SERVICE $DEST
 sudo cp $SPLASH_HALTED_SERVICE $DEST
+sudo cp $SPLASH_FINAL_SERVICE $DEST
 
 sudo systemctl daemon-reload
 sudo systemctl enable $STATUS_SERVICE
@@ -23,5 +26,8 @@ sudo systemctl start $SPLASH_SHUTTINGDOWN_SERVICE
 
 sudo systemctl enable $SPLASH_HALTED_SERVICE
 sudo systemctl start $SPLASH_HALTED_SERVICE
+
+sudo systemctl enable $SPLASH_FINAL_SERVICE
+sudo systemctl start $SPLASH_FINAL_SERVICE
 
 sudo systemctl restart amya-logo-2.service
